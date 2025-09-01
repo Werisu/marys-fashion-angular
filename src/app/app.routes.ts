cc9922d587f0ba89d507b9b0e45f6fd46495a44c
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2025-09-01 17:15:54 -0300

    feat(routes): update routing configuration to include home redirection
    - Added a redirect from the root path to the 'home' path for improved navigation.
    - Set pathMatch to 'full' to ensure the redirect works correctly.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { Route } from '@angular/router';
 export const appRoutes: Route[] = [
   {
     path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
     loadComponent: () =>
       import('@marys-fashion-angular/home').then((m) => m.Home),
   },
