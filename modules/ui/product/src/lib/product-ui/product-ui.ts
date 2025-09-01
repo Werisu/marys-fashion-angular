517e042c5c2227f1d406d4c9c0711e344fa22b83
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2025-09-01 17:19:40 -0300

    feat(product-ui): create product UI library with configuration and tests
    - Introduced the product-ui library, including core components, styles, and templates.
    - Added ESLint and Jest configurations for linting and testing.
    - Created TypeScript configuration files for library and testing.
    - Implemented a basic ProductUi component with a template and styles.
    - Included unit tests to verify component functionality.
    - Updated the main TypeScript configuration to include the new product-ui library path.

---
+++ modules/ui/product/src/lib/product-ui/product-ui.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-product-ui',
+  imports: [],
+  templateUrl: './product-ui.html',
+  styleUrl: './product-ui.css',
+})
+export class ProductUi {}
